f504a263be6b38e8c0c337c99591529be9b1e4cc
Larii2024 <53659954+Larii2024@users.noreply.github.com>
2020-09-09 20:26:03 +0530

    Updates

---
+++ precache-manifest.932bb54fd2065941126f09469401408d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7c9492741440573b42ddf668c9b5d4",
+    "url": "/Man_Power_Services/my-app/index.html"
+  },
+  {
+    "revision": "ed267d907ccd03204e1b",
+    "url": "/Man_Power_Services/my-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "44714a8a9557a18732fa",
+    "url": "/Man_Power_Services/my-app/static/css/main.392ab2c2.chunk.css"
+  },
+  {
+    "revision": "ed267d907ccd03204e1b",
+    "url": "/Man_Power_Services/my-app/static/js/2.601ffdd6.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/Man_Power_Services/my-app/static/js/2.601ffdd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44714a8a9557a18732fa",
+    "url": "/Man_Power_Services/my-app/static/js/main.fc81bee1.chunk.js"
+  },
+  {
+    "revision": "a9330409e0786287aa9a",
+    "url": "/Man_Power_Services/my-app/static/js/runtime-main.a3207a10.js"
+  },
+  {
+    "revision": "2bb71ff808eaaac8914e6db33d25e371",
+    "url": "/Man_Power_Services/my-app/static/media/5.2bb71ff8.jpg"
+  },
+  {
+    "revision": "01a5d86ab459f7b35a176ab2c526e5f5",
+    "url": "/Man_Power_Services/my-app/static/media/6.01a5d86a.png"
+  },
+  {
+    "revision": "62fd37ef3203a999f7b9cfdaa4911e86",
+    "url": "/Man_Power_Services/my-app/static/media/attbg.62fd37ef.png"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/Man_Power_Services/my-app/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/Man_Power_Services/my-app/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "e3bca7666445db57e45d3413c7cbb0df",
+    "url": "/Man_Power_Services/my-app/static/media/unknownman.e3bca766.jpg"
+  }
+]);
